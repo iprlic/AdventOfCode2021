c0fc9cb1f14f2ae7c30fa61f04311f64e0a69355
Ivan Prlic <iprlic@gmail.com>
2021-12-03 08:59:42 +0100

    Day 3

---
+++ day03/part1.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path('input.txt', __dir__)
+input = File.read(file_path)
+
+lines = input.split("\n")
+
+half = lines.size/2
+sums = Array.new(lines.first.size, 0)
+gamma = Array.new(lines.first.size, 0)
+epsilon = Array.new(lines.first.size, 0)
+
+lines.each do |l|
+    digits = l.chars
+
+    digits.each_with_index { |d, i| sums[i] += d.to_i }
+end
+
+
+
+sums.each_with_index do |d, i| 
+    gamma[i] = d >= half ? 1 : 0
+    epsilon[i] = gamma[i] == 1 ? 0 : 1
+end
+
+puts epsilon.join.to_i(2) * gamma.join.to_i(2)
---
+++ day03/part2.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+def common(arr, pos, most)
+    half = arr.size/2.to_f
+    sum = arr.inject(0) { |s,l| s + l[pos] }
+
+    case
+    when sum > half
+        most ? 1 : 0
+    when sum < half
+        most ? 0 : 1
+    else
+        most ? 1 : 0
+    end
+end
+
+def reduction(arr, pos, most)
+    return arr if arr.size == 1
+
+    cm = common(arr, pos, most)
+    reduction(arr.select { |a| cm == a[pos]}, pos+1, most)
+end
+
+
+file_path = File.expand_path('input.txt', __dir__)
+input = File.read(file_path)
+
+lines = input.split("\n").map(&:chars).map { |l| l.map(&:to_i) }
+
+
+og = reduction(lines, 0, true)
+co = reduction(lines, 0, false)
+
+puts og.join.to_i(2) * co.join.to_i(2)
