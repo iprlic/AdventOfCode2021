65bba89810a5558e50f621d27f598e6c99d734c6
Ivan Prlic <iprlic@gmail.com>
2021-12-05 10:00:59 +0100

    Day 5

---
+++ day05/part1.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+file_path = File.expand_path('input.txt', __dir__)
+input = File.read(file_path)
+
+lines = input.split("\n")
+
+points = {}
+
+lines.each do |l|
+  line = l.split(' -> ').map { |x| x.split(',').map(&:to_i) }
+
+  if line[0][0] == line[1][0]
+    range = line[0][1] < line[1][1] ? (line[0][1]..line[1][1]).to_a : (line[1][1]..line[0][1]).to_a
+    range.each do |i|
+      point = "#{line[0][0]},#{i}"
+      points[point] = points[point] ? points[point] + 1 : 1
+    end
+  end
+
+  next unless line[0][1] == line[1][1]
+
+  range = line[0][0] < line[1][0] ? (line[0][0]..line[1][0]).to_a : (line[1][0]..line[0][0]).to_a
+  range.each do |i|
+    point = "#{i},#{line[0][1]}"
+    points[point] = points[point] ? points[point] + 1 : 1
+  end
+end
+
+cnt = 0
+
+points.each do |_k, v|
+  cnt += 1 if v >= 2
+end
+
+puts cnt
