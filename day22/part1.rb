9c1d7566bc8f469741d573acb7fdc7e271517368
Ivan Prlic <iprlic@gmail.com>
2021-12-22 10:18:13 +0100

    Day22 rubocop

--- day22/part1.rb
+++ day22/part1.rb
@@ -1,92 +1,93 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
 require 'set'
 
 file_path = File.expand_path('input.txt', __dir__)
 
-steps = File.read(file_path).split("\n").map do |a| 
-    on = a.split(' ').first == 'on'
+steps = File.read(file_path).split("\n").map do |a|
+  on = a.split(' ').first == 'on'
 
-    c = a.split(' ').last.split(',').map { |b|
-        b.slice(2, b.size-1).split('..').map(&:to_i)
-    }
+  c = a.split(' ').last.split(',').map do |b|
+    b.slice(2, b.size - 1).split('..').map(&:to_i)
+  end
 
-    x1 = c[0][0]
-    x2 = c[0][1]
+  x1 = c[0][0]
+  x2 = c[0][1]
 
-    y1 = c[1][0]
-    y2 = c[1][1]
+  y1 = c[1][0]
+  y2 = c[1][1]
 
-    z1 = c[2][0]
-    z2 = c[2][1]
+  z1 = c[2][0]
+  z2 = c[2][1]
 
-    {
-        'x1' => x1,
-        'x2' => x2,
-        'y1' => y1,
-        'y2' => y2,
-        'z1' => z1,
-        'z2' => z2,
-        'on' => on
-    }
+  {
+    'x1' => x1,
+    'x2' => x2,
+    'y1' => y1,
+    'y2' => y2,
+    'z1' => z1,
+    'z2' => z2,
+    'on' => on
+  }
 end
 
 ons = Set.new
 
 steps.each do |s|
-    x1 = -50
-    x2 = 50
+  x1 = -50
+  x2 = 50
 
-    y1 = -50
-    y2 = 50
+  y1 = -50
+  y2 = 50
 
-    z1 = -50
-    z2 = 50
+  z1 = -50
+  z2 = 50
 
-    if s['x1'] >= -50 and s['x1'] <= 50
-        x1 = s['x1']
-    elsif s['x1'] > 50
-        next
+  if (s['x1'] >= -50) && (s['x1'] <= 50)
+    x1 = s['x1']
+  elsif s['x1'] > 50
+    next
+  end
+
+  if (s['x2'] >= -50) && (s['x2'] <= 50)
+    x2 = s['x2']
+  elsif s['x2'] < -50
+    next
+  end
+
+  if (s['y1'] >= -50) && (s['y1'] <= 50)
+    y1 = s['y1']
+  elsif s['y1'] > 50
+    next
+  end
+
+  if (s['y2'] >= -50) && (s['y2'] <= 50)
+    y2 = s['y2']
+  elsif s['y2'] < -50
+    next
+  end
+
+  if (s['z1'] >= -50) && (s['z1'] <= 50)
+    z1 = s['z1']
+  elsif s['z1'] > 50
+    next
+  end
+
+  if (s['z2'] >= -50) && (s['z2'] <= 50)
+    z2 = s['z2']
+  elsif s['z2'] < -50
+    next
+  end
+
+  (x1..x2).each do |x|
+    (y1..y2).each do |y|
+      (z1..z2).each do |z|
+        ons.add([x, y, z]) if s['on']
+        ons.delete([x, y, z]) unless s['on']
+      end
     end
-
-    if s['x2'] >= -50 and s['x2'] <= 50
-        x2 = s['x2']
-    elsif s['x2'] < -50
-        next
-    end
-
-    if s['y1'] >= -50 and s['y1'] <= 50
-        y1 = s['y1']
-    elsif s['y1'] > 50
-        next
-    end
-
-    if s['y2'] >= -50 and s['y2'] <= 50
-        y2 = s['y2']
-    elsif s['y2'] < -50
-        next
-    end
-
-    if s['z1'] >= -50 and s['z1'] <= 50
-        z1 = s['z1']
-    elsif s['z1'] > 50
-        next
-    end
-
-    if s['z2'] >= -50 and s['z2'] <= 50
-        z2 = s['z2']
-    elsif s['z2'] < -50
-        next
-    end
-
-    (x1..x2).each do |x|
-        (y1..y2).each do |y|
-            (z1..z2).each do |z|
-                ons.add([x,y,z]) if s['on']
-                ons.delete([x,y,z]) if !s['on']
-            end
-        end
-    end
+  end
 end
 
 puts ons.size
--- day22/part2.rb
+++ day22/part2.rb
@@ -1,71 +1,72 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
 require 'set'
 
 file_path = File.expand_path('input.txt', __dir__)
 
-steps = File.read(file_path).split("\n").map do |a| 
-    on = a.split(' ').first == 'on'
+steps = File.read(file_path).split("\n").map do |a|
+  on = a.split(' ').first == 'on'
 
-    c = a.split(' ').last.split(',').map { |b|
-        b.slice(2, b.size-1).split('..').map(&:to_i)
-    }
+  c = a.split(' ').last.split(',').map do |b|
+    b.slice(2, b.size - 1).split('..').map(&:to_i)
+  end
 
-    x1 = c[0][0]
-    x2 = c[0][1]
+  x1 = c[0][0]
+  x2 = c[0][1]
 
-    y1 = c[1][0]
-    y2 = c[1][1]
+  y1 = c[1][0]
+  y2 = c[1][1]
 
-    z1 = c[2][0]
-    z2 = c[2][1]
+  z1 = c[2][0]
+  z2 = c[2][1]
 
-    {
-        'x1' => x1,
-        'x2' => x2,
-        'y1' => y1,
-        'y2' => y2,
-        'z1' => z1,
-        'z2' => z2,
-        'on' => on
-    }
+  {
+    'x1' => x1,
+    'x2' => x2,
+    'y1' => y1,
+    'y2' => y2,
+    'z1' => z1,
+    'z2' => z2,
+    'on' => on
+  }
 end
 
 def overlap(c1, c2)
-    c = {
-        'x1' => [c2['x1'],c1['x1']].max,
-        'x2' => [c2['x2'],c1['x2']].min,
-        'y1' => [c2['y1'],c1['y1']].max,
-        'y2' => [c2['y2'],c1['y2']].min,
-        'z1' => [c2['z1'],c1['z1']].max,
-        'z2' => [c2['z2'],c1['z2']].min
-    }
+  c = {
+    'x1' => [c2['x1'], c1['x1']].max,
+    'x2' => [c2['x2'], c1['x2']].min,
+    'y1' => [c2['y1'], c1['y1']].max,
+    'y2' => [c2['y2'], c1['y2']].min,
+    'z1' => [c2['z1'], c1['z1']].max,
+    'z2' => [c2['z2'], c1['z2']].min
+  }
 
-    return c if c['x1'] <= c['x2'] and c['y1'] <= c['y2'] and c['z1'] <= c['z2']
-    nil
+  return c if (c['x1'] <= c['x2']) && (c['y1'] <= c['y2']) && (c['z1'] <= c['z2'])
+
+  nil
 end
 
-
 def area(c)
-    (c['x2'] - c['x1'] + 1) * (c['y2'] - c['y1'] + 1) * (c['z2'] - c['z1'] + 1)
+  (c['x2'] - c['x1'] + 1) * (c['y2'] - c['y1'] + 1) * (c['z2'] - c['z1'] + 1)
 end
 
 done = []
 cnt = 0
 
 steps.each do |s|
-    overlaps = []
+  overlaps = []
 
-    overlaps.push(s) if s['on']
-    
-    done.each do |d|
-        c = overlap(d,s)
-        next if c.nil?
-        c['on'] = !d['on']
-        overlaps.push(c)
-    end
-    done += overlaps
+  overlaps.push(s) if s['on']
+
+  done.each do |d|
+    c = overlap(d, s)
+    next if c.nil?
+
+    c['on'] = !d['on']
+    overlaps.push(c)
+  end
+  done += overlaps
 end
 
-
-puts done.reduce(0) { |s,c|  s + area(c) * (c['on'] ? 1 : -1)}
+puts done.reduce(0) { |s, c| s + area(c) * (c['on'] ? 1 : -1) }
